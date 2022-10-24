91d28c309ae101f8e409f06c9d623c75bf3a7b2a
Robbie Blaine <robbie@zenaptix.com>
2022-10-24 15:54:53 +0200

    Meditations - reqwest

--- meditations/src/main.rs
+++ meditations/src/main.rs
@@ -1,5 +1,3 @@
-use hyper::{body::Buf, Body, Client, Request};
-use hyper_tls::HttpsConnector;
 use serde_derive::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -10,21 +8,14 @@ struct Quote {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    let https = HttpsConnector::new();
-    let client = Client::builder().build(https);
     // https://stoicquotesapi.com/v1/api/quotes/random
     // https://stoic-wisdom.com/api
     let base_uri = "https://stoicquotesapi.com/v1/api/quotes".to_string();
     let uri = base_uri + "/random";
 
-    let req = Request::get(uri).body(Body::empty())?;
-    let res = client.request(req).await?;
+    let resp = reqwest::get(uri).await?.json::<Quote>().await?;
 
-    let body = hyper::body::aggregate(res).await?;
-
-    let quote: Quote = serde_json::from_reader(body.reader())?;
-
-    println!("\"{}\" - {}", quote.body, quote.author);
+    println!("\"{}\" - {}", resp.body, resp.author);
 
     Ok(())
 }
